8b5a3a46a5ca4473d1c6752d3d97321e6314f542
candyzp <166943815+candyzp@users.noreply.github.com>
2026-09-21 13:55:42 -0400

    iOS: isolate GPU buffer allocation from stale GL errors

--- src/renderer/ios/BufferIOS.cpp
+++ src/renderer/ios/BufferIOS.cpp
@@ -60,9 +60,16 @@ Buffer* Buffer::create(const char* name, usize size, GLenum usage, bool keepShad
 
     GLint previouslyBoundBuffer = 0;
     glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &previouslyBoundBuffer);
+
+    // OpenGL errors are sticky. An unrelated error from stock GD or another mod
+    // must not make a successful Bismuth allocation look like it failed.
+    while (glGetError() != GL_NO_ERROR) {}
+
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, size, nullptr, usage);
-    const GLenum allocationError = glGetError();
+    GLenum allocationError = GL_NO_ERROR;
+    while (const GLenum error = glGetError(); error != GL_NO_ERROR)
+        allocationError = error;
     glBindBuffer(GL_ARRAY_BUFFER, previouslyBoundBuffer);
 
     if (allocationError != GL_NO_ERROR) {
